c8e7ad422fae85e006e3886a8cb7bfad288e07f0
Augusto Arraes <augustobarraes@gmail.com>
2023-02-20 12:41:41 -0500

    finished lesson 17

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,9 @@
 class Movie < ApplicationRecord
+
+  def self.released
+    Movie.where("released_on > ?", 1500).order("released_on DESC")
+  end
+
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
